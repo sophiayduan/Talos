b1695101f37985f4071efe73204e9098d63538fd
SiaBob <142806607+SiaBob@users.noreply.github.com>
2025-02-10 06:34:04 -0800

    animations

---
+++ Assets/Character controller/scripts/PlayerAnimation.cs
@@ -0,0 +1,34 @@
+using Charactercontroller.Inputs;
+using UnityEngine;
+
+namespace Charactercontroller{
+    public class PlayerAnimation : MonoBehaviour
+    {
+        [SerializeField] private Animator _animator;
+        [SerializeField] private float locomotionBlendSpeed = 0.02f;
+
+        private PlayerInputs _playerinputs;
+        private static int inputXHash = Animator.StringToHash("inputX");
+        private static int inputYHash = Animator.StringToHash("inputY");
+        private Vector3 _currentBlendInput = Vector3.zero;
+
+        private void Awake(){
+            _playerinputs = GetComponent<PlayerInputs>();
+        }
+
+        private void Update(){
+            UpdateAnimationState();
+        }
+
+        private void UpdateAnimationState(){
+            Vector2 inputTarget = _playerinputs.MovementInput;
+            _currentBlendInput = Vector3.Lerp(_currentBlendInput, inputTarget, locomotionBlendSpeed * Time.deltaTime);
+
+            _animator.SetFloat(inputXHash, inputTarget.x);
+            _animator.SetFloat(inputYHash, inputTarget.y);
+
+        }
+    }
+
+}
+
