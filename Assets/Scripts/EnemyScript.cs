33a5ba4d3cf1cd9ff741537a6aad0b05f891207c
Sophia Duan <duansophia8@gmail.com>
2025-02-28 21:08:09 -0500

    Revert "tried animation"
    This reverts commit 6d7ab2254ab475cd3ec11b58de1c1d304862fe0b.

--- Assets/Scripts/EnemyScript.cs
+++ Assets/Scripts/EnemyScript.cs
@@ -29,9 +29,7 @@ public class EnemyScript : MonoBehaviour
 
     public float LastAttack;
     public GameObject EnemyBullet;
-    public Vector3 Direction;
-    public Vector3 Heading;
-    public float Distance;
+
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -66,10 +64,10 @@ public class EnemyScript : MonoBehaviour
                     seePlayer = false;
                 }
                 else {
-                    
-                    Heading = Target.transform.position - transform.position;
-                    Distance = Heading.magnitude;
-                    Direction = Heading / Distance;
+
+                    var Heading = Target.transform.position - transform.position;
+                    var Distance = Heading.magnitude;
+                    var Direction = Heading / Distance;
 
                     Vector3 Move = new Vector3(Direction.x *Speed, 0, Direction.z * Speed);
 
