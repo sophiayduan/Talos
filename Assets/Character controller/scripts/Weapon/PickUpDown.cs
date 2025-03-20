25051cf33e55961272a37fde506d3e538ea30575
SiaBob <142806607+SiaBob@users.noreply.github.com>
2025-03-19 21:41:19 -0700

    wait

---
+++ Assets/Character controller/scripts/Weapon/PickUpDown.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+namespace Charactercontroller.scripts.Weapons{
+    public class PickUpDown : MonoBehaviour
+    {
+        [SerializeField] private Transform playerCameraTranfrom;
+        [SerializeField] private LayerMask pickUpLayerMask;
+        private void Update()
+        {
+            if(Input.GetKeyDown(KeyCode.E)){
+                float pickUpDistance = 2f;
+                if(Physics.Raycast(transform.position, transform.forward, out RaycastHit raycastHit, pickUpDistance, pickUpLayerMask)){
+                    Debug.Log(raycastHit.transform);
+                }
+            }
+        }
+    }
+}
+
