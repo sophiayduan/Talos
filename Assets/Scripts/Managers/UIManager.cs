c504f147ab44452dff5c03141849b47f7515ab25
Sophia Duan <duansophia8@gmail.com>
2025-03-21 23:56:06 -0400

    add unity particlepack

--- Assets/Scripts/Managers/UIManager.cs
+++ Assets/Scripts/Managers/UIManager.cs
@@ -5,40 +5,42 @@ public class UIManager : MonoBehaviour
     [SerializeField] GameObject deathPanel;
     [SerializeField] GameObject pausePanel;
     [SerializeField] GameObject settingsPanel;
-    
-    private bool isPaused = false;
 
-    public void ToggleDeathPanel() {
+
+    private bool isPaused = false;
+    private GameObject[] respawns;
+    void Start()
+    {
+        respawns = GameObject.FindGameObjectsWithTag("Respawn");
+    }
+    public void ToggleDeathPanel() 
+    {
         if (deathPanel.activeSelf == true){
             return;
         }
         else deathPanel.SetActive(true);
     }
-    public void TogglePausePanel() {
+    public void TogglePausePanel() 
+    {
         isPaused = !isPaused;
         pausePanel.SetActive(!pausePanel.activeSelf);
-        GameObject[] respawns = GameObject.FindGameObjectsWithTag("Respawn");
+        foreach (GameObject obj in respawns){
+            obj.SetActive(!obj.activeSelf);
 
-        if (isPaused == true){
-            Time.timeScale = 0f;
-            foreach (GameObject obj in respawns){
-            obj.SetActive(false);
-        }        
-        }
-        else {
-            Time.timeScale = 1f;            
-            foreach (GameObject obj in respawns){
-                obj.SetActive(true);
-            }
-        }
+        if (isPaused == true)Time.timeScale = 0f;
+              
+        else Time.timeScale = 1f;            
+
+        
     }
-        public void ToggleSettingsPanel() {
+}
+        public void ToggleSettingsPanel() 
+        {
             if (SceneManager.GetActiveScene().buildIndex == 1){
                 pausePanel.SetActive(!pausePanel.activeSelf);
                 settingsPanel.SetActive(!settingsPanel.activeSelf);
             }
-            else if (SceneManager.GetActiveScene().buildIndex == 2) {
-                settingsPanel.SetActive(!settingsPanel.activeSelf);
-            }
+            else if (SceneManager.GetActiveScene().buildIndex == 2) settingsPanel.SetActive(!settingsPanel.activeSelf);
+            
     }
 }
