c9ec67f527f296d6f907d619b81aff33b3c0c46a
Sophia Duan <duansophia8@gmail.com>
2025-03-28 14:17:11 -0400

    Revert "Merge remote-tracking branch 'origin/improving-things'"
    This reverts commit edb6d4eeb708151fd820001848d5c4a0e9be9fdb, reversing
    changes made to 15cc4ea333ea5172535bd17210b6304c391b960b.

--- Assets/Character controller/scripts/PlayerAnimation.cs
+++ Assets/Character controller/scripts/PlayerAnimation.cs
@@ -1,6 +1,5 @@
 using System.Linq;
 using Charactercontroller.Inputs;
-using Charactercontroller.scripts;
 using UnityEngine;
 
 namespace Charactercontroller{
@@ -13,7 +12,6 @@ namespace Charactercontroller{
         private PlayerState _playerState;
         private PlayerController _playerController;
         private PlayerActionInputs _playerActionInputs;
-        private PickUpDown _pickUpDown;
 
         private static int inputXHash = Animator.StringToHash("inputX");
         private static int inputYHash = Animator.StringToHash("inputY");
@@ -25,8 +23,6 @@ namespace Charactercontroller{
         private static int isPunchingHash = Animator.StringToHash("isPunching");
         private static int isGrabingHash = Animator.StringToHash("isGrabing");
         private static int isPlayingActionsHash = Animator.StringToHash("isPlayingAction");
-        private static int isAimingHash = Animator.StringToHash("isAiming");
-        private static int isShootingHash = Animator.StringToHash("isShooting");
         private int[] actionHashes;
         private static int isRotatingToTargetHash = Animator.StringToHash("isRotatingToTarget");
         private static int rotationMismatchHash = Animator.StringToHash("rotationMismatch");
@@ -43,9 +39,8 @@ namespace Charactercontroller{
             _playerState = GetComponent<PlayerState>();
             _playerController = GetComponent<PlayerController>();
             _playerActionInputs = GetComponent<PlayerActionInputs>();
-            _pickUpDown = GetComponent<PickUpDown>();
 
-            actionHashes = new int[] {isGrabingHash, isFallingHash};
+            actionHashes = new int[] {isGrabingHash};
         }
 
         private void Update(){
@@ -57,10 +52,9 @@ namespace Charactercontroller{
             bool isRunning = _playerState.CurrentPlayerMovementState == PlayerMovementState.Running;
             bool isSprintng = _playerState.CurrentPlayerMovementState == PlayerMovementState.Sprinting;
             bool isJumping = _playerState.CurrentPlayerMovementState == PlayerMovementState.Jumping;
+            bool isFalling = _playerState.CurrentPlayerMovementState == PlayerMovementState.Falling;
             bool isGrounded = _playerState.InGroundedState();
-            bool isFalling = _playerState.CurrentPlayerMovementState == PlayerMovementState.Falling && !isGrounded;
             bool isPlayingAction = actionHashes.Any(hash => _animator.GetBool(hash));
-            bool isShooting = _playerActionInputs.AttackPressed;
 
             bool isRunBlendValue = isRunning || isJumping || isFalling;
 
@@ -81,8 +75,7 @@ namespace Charactercontroller{
             _animator.SetFloat(rotationMismatchHash, _playerController.RotationMismatch);
             _animator.SetBool(isPunchingHash, _playerActionInputs.AttackPressed);
             _animator.SetBool(isGrabingHash, _playerActionInputs.GrabPressed);
-            _animator.SetBool(isAimingHash, _pickUpDown.isAiming);
-            _animator.SetBool(isShootingHash, _pickUpDown.isShooting);
+
         }
     }
 
